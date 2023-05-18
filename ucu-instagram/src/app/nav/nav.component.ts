71437158ff7d0fa596737c9483a9fed1b0be1638
NicoLorenzo1 <88661584+NicoLorenzo1@users.noreply.github.com>
2023-05-17 21:08:41 -0300

    NavBar

--- ucu-instagram/src/app/nav/nav.component.ts
+++ ucu-instagram/src/app/nav/nav.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { Input } from '@angular/core';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
-
+@Input() selected! : "discover" | "search"  | "upload" | "chat" | "profile" ;
 }
