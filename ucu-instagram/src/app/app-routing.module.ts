a0641524c8202008b151300055dff08578746b41
[Franco Barlocco] <[titobarlocco52@gmail.com]>
2023-05-21 12:49:45 -0300

    Upload Picture

--- ucu-instagram/src/app/app-routing.module.ts
+++ ucu-instagram/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UploadComponent } from './upload/upload.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: "upload", component: UploadComponent}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })], // <-- Habilitar el modo de hash
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
