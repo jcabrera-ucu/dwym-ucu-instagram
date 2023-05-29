c160467c340a21d873000cbd750120e03d8b6cfb
Juan Cabrera <j.cabrera@correo.ucu.edu.uy>
2023-05-29 18:42:56 -0300

    Fix route list style

--- ucu-instagram/src/app/app-routing.module.ts
+++ ucu-instagram/src/app/app-routing.module.ts
@@ -4,7 +4,12 @@ import { UploadComponent } from './upload/upload.component';
 import { AppComponent } from './app.component';
 import { PostContainerComponent } from './post-container/post-container.component';
 
-const routes: Routes = [{path: "upload", component: UploadComponent},{path: "discover", component: PostContainerComponent},{path: "**", redirectTo: "discover"}];
+const routes: Routes = [
+  {path: "upload", component: UploadComponent},
+  {path: "discover", component: PostContainerComponent},
+  {path: "**", redirectTo: "discover"}
+];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
